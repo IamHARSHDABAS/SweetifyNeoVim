dbe6cc00235f92354e4e94676b0cfa18e6ca6336
IamHARSHDABAS <iamharshdabas@gmail.com>
2023-03-06 14:28:39 +0530

    Updated colors

--- lua/sweetify/palette.lua
+++ lua/sweetify/palette.lua
@@ -2,8 +2,8 @@
 return {
 	bg        = "#0D001A",
 	fg        = "#8000FF",
-	selection = "#190033",
-	comment   = "#F2E5FF",
+	selection = "#26004D",
+	comment   = "#400080",
 	black     = "#0D001A",
 	red       = "#FF0000",
 	green     = "#FF8000",
